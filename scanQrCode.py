b48deb524244a7528e1fdce92a3186567451f379
unknown <ahedqaood19@gmail.com>
2021-03-31 03:22:58 +0300

    add mask code simulation

--- scanQrCode.py
+++ scanQrCode.py
@@ -7,69 +7,104 @@ import cv2
 import re
 
 checkForMask = False
+timeIn = time.time()
+waitMaskDuration = 5
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+def main():
+	# initialize the video stream and allow the camera sensor to warm up
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
 
-# loop over the frames from the video stream
-while True:
-	# grab the frame from the threaded video stream and resize it to
-	# have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=600)
-	
-	hsv = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2HSV)
-	(h, s, v) = cv2.split(hsv)
+	# loop over the frames from the video stream
+	while True:
+		global checkForMask
+		global timeIn
 
-	blurreds = cv2.GaussianBlur(s, (11, 11), 0)
-	blurredv = cv2.GaussianBlur(v, (11, 11), 0)
+		# grab the frame from the threaded video stream and resize it to
+		# have a maximum width of 400 pixels
+		frame = vs.read()
+		frame = imutils.resize(frame, width=600)
+		
+		hsv = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2HSV)
+		(h, s, v) = cv2.split(hsv)
 
-	masks = cv2.inRange(blurreds, 230, 255)
-	maskv = cv2.inRange(blurredv, 110, 150)
-	maskvWhite = cv2.inRange(blurredv, 150, 255)
+		blurreds = cv2.GaussianBlur(s, (11, 11), 0)
+		blurredv = cv2.GaussianBlur(v, (11, 11), 0)
 
-	bitwiseAnd = cv2.bitwise_and(masks, maskv)
-	bitwiseOr = cv2.bitwise_or(bitwiseAnd, maskvWhite)
+		masks = cv2.inRange(blurreds, 230, 255)
+		maskv = cv2.inRange(blurredv, 110, 150)
+		maskvWhite = cv2.inRange(blurredv, 150, 255)
 
-	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11,11))
-	closing = cv2.morphologyEx(bitwiseOr, cv2.MORPH_CLOSE, kernel)
-	opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
-	closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+		bitwiseAnd = cv2.bitwise_and(masks, maskv)
+		bitwiseOr = cv2.bitwise_or(bitwiseAnd, maskvWhite)
 
-	green = cv2.bitwise_or(frame, frame, mask=closing)
-	gray = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
-	(T, qrCode) = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY_INV)
-	barcodes = pyzbar.decode(qrCode)
-	# loop over the detected barcodes
-	for barcode in barcodes:
-		# extract the bounding box location of the barcode and draw the
-		# bounding box surrounding the barcode on the image
-		(x, y, w, h) = barcode.rect
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-		# the barcode data is a bytes object so if we want to draw it on
-		# our output image we need to convert it to a string first
-		barcodeData = barcode.data.decode("utf-8")
-		barcodeType = barcode.type
-		# draw the barcode data and barcode type on the image
-		text = "{}".format(barcodeData)
-		cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-			0.5, (0, 0, 255), 2)
+		kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11,11))
+		closing = cv2.morphologyEx(bitwiseOr, cv2.MORPH_CLOSE, kernel)
+		opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
+		closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
-		regex_result = re.search("[\w\d\.\*\\=*?*]", barcodeData)
+		green = cv2.bitwise_or(frame, frame, mask=closing)
+		gray = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
+		(T, qrCode) = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY_INV)
+		barcodes = pyzbar.decode(qrCode)
+		# loop over the detected barcodes
+		for barcode in barcodes:
+			# extract the bounding box location of the barcode and draw the
+			# bounding box surrounding the barcode on the image
+			(x, y, w, h) = barcode.rect
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+			# the barcode data is a bytes object so if we want to draw it on
+			# our output image we need to convert it to a string first
+			barcodeData = barcode.data.decode("utf-8")
+			barcodeType = barcode.type
+			# draw the barcode data and barcode type on the image
+			text = "{}".format(barcodeData)
+			cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 0, 255), 2)
 
-		# print the barcode type and data to the terminal
-		# print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
-		# print("[INFO] Found regex: {} {} characters".format(barcodeData, len(barcodeData)))
+			regex_result = re.findall("[\w\d\.\*\\=*?*]", barcodeData)
 
-	cv2.imshow('QR Code Scanner', frame)
-	key = cv2.waitKey(1) & 0xFF
+			# print the barcode type and data to the terminal
+			# print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+			# print("[INFO] Found regex: {} {} characters".format(barcodeData, len(regex_result)))
+
+			if len(regex_result) == 44:
+				print("[INFO] Pass")
+				checkForMask = True
+		
+		if checkForMask:
+			checkForMask = False
+			timeIn = time.time()
+			maskCode()
+
+		cv2.imshow('QR Code Scanner', frame)
+		key = cv2.waitKey(1) & 0xFF
+
  
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			print("[INFO] cleaning up...")
+			cv2.destroyAllWindows()
+			vs.stop()
+			break
 
-print("[INFO] cleaning up...")
-cv2.destroyAllWindows()
-vs.stop()
+def maskCode():
+	global timeIn
+	global waitMaskDuration
+
+	print("Start Mask Code!")
+	print(time.time())
+	
+	while True:
+		print(time.time() - timeIn)
+		print(time.time() - timeIn > waitMaskDuration)
+		if time.time() - timeIn > waitMaskDuration:
+			break
+
+		print("Mask code!")
+	
+	print("End of Mask code")
+
+if __name__ == "__main__":
+	main()
