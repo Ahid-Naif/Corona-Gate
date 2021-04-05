4e8379f195f2682021395062abd1f31a4b4d2f65
unknown <ahedqaood19@gmail.com>
2021-04-05 08:33:00 +0300

    add buzzer code

---
+++ buzzer.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+
+def main():
+
+    while True:
+        buzzerOn(2)
+        buzzerOff(2)
+
+def buzzerOff(waitTime):
+   
+    #
+    time.sleep(waitTime)
+    #
+
+def buzzerOn(waitTime):
+   
+    #
+    time.sleep(waitTime)
+    #
+
+if __name__ == "__main__":
+	main()
