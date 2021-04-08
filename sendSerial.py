e791779be0852ed67e6bd69319b54e8919794bd8
unknown <ahedqaood19@gmail.com>
2021-04-08 22:17:18 +0300

    add code

--- sendSerial.py
+++ sendSerial.py
@@ -5,7 +5,17 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        ser.write(b"red\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
-        time.sleep(1)
+        time.sleep(2)
+
+		ser.write(b"green\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(2)
+
+		ser.write(b"white!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(2)
