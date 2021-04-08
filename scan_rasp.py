1c8ae7ebbd10c3cc284f05e49f04a5192dba0bf6
unknown <ahedqaood19@gmail.com>
2021-04-08 23:24:23 +0300

    updates

--- scan_rasp.py
+++ scan_rasp.py
@@ -7,7 +7,12 @@ import cv2
 import re
 from keras.models import load_model
 import numpy as np
+import serial
+from smbus2 import SMBus
+from mlx90614 import MLX90614
 import RPi.GPIO as GPIO
+
+bus = SMBus(1)
 
 #
 #servo
@@ -17,16 +22,6 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 pwm11=GPIO.PWM(11, 50)
 pwm11.start(0)
-
-# LEDs
-GPIO.setup(38, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(36, GPIO.OUT, initial=GPIO.LOW) 
-GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
-
-# buzzer
-GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW) 
-
-#
 
 model=load_model("./model2-005.model")
 
@@ -46,10 +41,14 @@ print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
+
 def main():
 	global checkForMask
 	global timeIn
 	global vs
+	global ser
 	global num_people
 
 	closeGate()
@@ -57,8 +56,8 @@ def main():
 
 	# loop over the frames from the video stream
 	while True:
-		buzzerOff()
-		white()
+		ser.write(b"buzzerOFF\n")
+		ser.write(b"white\n")
 		# grab the frame from the threaded video stream and resize it to
 		# have a maximum width of 400 pixels
 		frame = vs.read()
@@ -106,13 +105,14 @@ def main():
 			regex_result = re.findall("[\w\d\.\*\\=*?*+*-*/*]", barcodeData)
 
 			if len(regex_result) == 44:
-				print("[INFO] Pass")
+				print("[INFO] QR Code Pass")
 				checkForMask = True
 		
 		if checkForMask:
 			checkForMask = False
 			cv2.destroyAllWindows()
-			buzzerOn(2)
+			ser.write(b"buzzerON\n")
+			time.sleep(2)
 			timeIn = time.time()
 			maskCode()
 
@@ -135,6 +135,7 @@ def maskCode():
 	global timeIn
 	global waitMaskDuration
 	global vs
+	global ser
 	global num_people
 
 	size = 4
@@ -142,12 +143,17 @@ def maskCode():
 	isMaskOn = False
 	isBreak = False
 
+	sensor = MLX90614(bus, address=0x5A)
+
 	while True:
-		buzzerOff()
-		white()
+		ser.write(b"buzzerOFF\n")
+		ser.write(b"white\n")
 		if time.time() - timeIn > waitMaskDuration:
 			cv2.destroyAllWindows()
-			red_buzzer(2)
+			# red & buzzer
+			ser.write(b"buzzerON\n")
+			ser.write(b"red\n")
+			time.sleep(2)
 			break
 
 		frame = vs.read()
@@ -185,18 +191,20 @@ def maskCode():
 					maskTime = time.time()
 			else:
 				if time.time() - maskTime > 1:
-					if(labels_dict[label] == 'mask'):
+					if(labels_dict[label] == 'mask' && (sensor.get_object_1() > 27 || sensor.get_object_1() < 40) ):
 						isMaskOn = False
 						isBreak = True
 						num_people = num_people + 1
-						openGate_buzzer_green(10)
+						openGate()
+						ser.write(b"buzzerON\n")
+						ser.write(b"green\n")
+						time.sleep(10)
 						closeGate()
 						break
 					else:
 						isMaskOn = False
 					
 					
-
 			cv2.rectangle(frame,(x,y),(x+w,y+h),color_dict[label],2)
 			cv2.rectangle(frame,(x,y-40),(x+w,y),color_dict[label],-1)
 			cv2.putText(frame, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
@@ -230,7 +238,7 @@ def closeGate():
 	GPIO.output(11, False)
 	pwm11.ChangeDutyCycle(0)
 
-def openGate_buzzer_green(waitTime):
+def openGate():
 	angle = 180
 	duty = angle / 18 + 2
 	GPIO.output(11, True)
@@ -238,47 +246,6 @@ def openGate_buzzer_green(waitTime):
 	time.sleep(1)
 	GPIO.output(11, False)
 	pwm11.ChangeDutyCycle(0)
-	#
-	GPIO.output(37, GPIO.HIGH) # buzzer
-	# green
-	GPIO.output(38, GPIO.LOW)
-	GPIO.output(36, GPIO.HIGH)
-	GPIO.output(32, GPIO.LOW)
-	#
-	time.sleep(waitTime)
-	#
-
-def buzzerOn(waitTime):
-	GPIO.output(37, GPIO.HIGH)
-	#
-	time.sleep(waitTime)
-	#
-
-def buzzerOff():
-	GPIO.output(37, GPIO.LOW)
-
-def red_buzzer(waitTime):
-	GPIO.output(38, GPIO.HIGH)
-	GPIO.output(36, GPIO.LOW)
-	GPIO.output(32, GPIO.LOW)
-	#
-	GPIO.output(37, GPIO.HIGH) # buzzer
-	#
-	time.sleep(waitTime)
-	#
-
-def green(waitTime):
-	GPIO.output(38, GPIO.LOW)
-	GPIO.output(36, GPIO.HIGH)
-	GPIO.output(32, GPIO.LOW)
-	#
-	time.sleep(waitTime)
-	#
-
-def white():
-	GPIO.output(38, GPIO.HIGH)
-	GPIO.output(36, GPIO.HIGH)
-	GPIO.output(32, GPIO.HIGH)
 		
 
 if __name__ == "__main__":
