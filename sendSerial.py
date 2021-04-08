a8266eb1b57441b367a09552257be81bb9ab5f32
unknown <ahedqaood19@gmail.com>
2021-04-08 21:46:00 +0300

    add code

--- sendSerial.py
+++ sendSerial.py
@@ -3,11 +3,12 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-ser.write('r'.encode())
-time.sleep(2)
+while True:
+	ser.write('r'.encode())
+	time.sleep(2)
 
-ser.write('g'.encode())
-time.sleep(2)
+	ser.write('g'.encode())
+	time.sleep(2)
 
-ser.write('w'.encode())
-time.sleep(2)
+	ser.write('w'.encode())
+	time.sleep(2)
