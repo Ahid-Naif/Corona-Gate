185d8235a959df3fc3b5c2a3c207248b882dcf82
unknown <ahedqaood19@gmail.com>
2021-04-09 16:30:03 +0300

    add code

--- scan_rasp.py
+++ scan_rasp.py
@@ -111,13 +111,6 @@ def main():
 		if checkForMask:
 			checkForMask = False
 			cv2.destroyAllWindows()
-			
-			done_image = np.zeros((400,400,3), np.uint8)
-			done_image[:,0:400] = (0,255,0)
-
-			cv2.putText(done_image, "Pass!", (100, 200), cv2.FONT_HERSHEY_SIMPLEX,
-						2, (255, 255, 255), 2)
-			cv2.imshow('Pass', done_image)
 
 			ser.write(b"buzzerON\n")
 			time.sleep(2)
